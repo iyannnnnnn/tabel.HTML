7cec5be37da4b7db674f757298bbd940da495c1b
iyannnnnnn <rifqiy83@gmail.com>
2025-01-15 11:15:07 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "Iyan";
+let alamat = "Sayung";
+let ttl = "Demak 23 Juli 2009";
+let gender = "Laki Laki";
+let agama = "Islam";
+let email = "rifqiy83@gmail.com";
+let berat = "53";
+let tinggi = "167"
+let hobi = "ngegame";
+let cita = "menjadi orang sukses";
+let pekerjaan = "siswa";
+let sekolah = "SMKN 1 SAYUNG";
+let jurusan = "PPLG";
+let usia = "15";
+
+document.write("nama saya adalah " + nama +"saya tinggal di" + alamat + "pada tahun 2024 usia saya adalah" + usia +"tahun,saya lahir di" + ttl + ",jenis kelamin saya adalah" + gender + ",saya beragama" + agama + ",alamat email saya adalah" + email + ",saya brat badan" + berat + "kg dengan tinggi" + tinggi + "cm,saya sangat senang" + hobi + "karena saya memiliki cita cita" + cita + "saat ini saya adalah seorang" + pekerjaan + "yang bersekolah di" + sekolah + "dan jurusan" + jurusan);
